653e228c021a5cea6180b92a6b88dc2fe2a5ce6d
Robert Gasparyan <robert.gasparyan@gmail.com>
2015-11-12 13:31:07 +0400

    Pass ID to cart

--- public/app.js
+++ public/app.js
@@ -1,7 +1,3 @@
-function say_hello() {
-
-    var x = window.localStorage.getItem('aaa')
-    x = x * 1 + 1
-    window.localStorage.setItem('aaa', x)
-    alert(x)
+function add_to_cart(id) {
+    alert("You added pizza with id" + id)
 }
